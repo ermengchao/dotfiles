cb437d793dcfd8306a951c3771fbeb057c036e77
ermengchao <zhangchao050530@gmail.com>
2026-01-20 01:32:16 +0800

    🚚 (*): move the unnecessary dotfiles to the archive folder

--- home/dot_config/nvim/lua/plugins/codesnap.lua
+++ home/dot_config/nvim/lua/plugins/codesnap.lua
@@ -6,18 +6,39 @@ return {
       require("codesnap").setup({
         show_line_number = true,
         snapshot_config = {
+          title = "",
+          theme = "candy",
+          background = "#00000000",
+          watermark = { content = "" },
           code_config = {
+            font_family = "SF Mono",
             breadcrumbs = {
               enable = true,
               separator = "/",
               color = "#80848b",
-              font_family = "CaskaydiaCove Nerd Font",
+              font_family = "SF Mono",
             },
           },
-          watermark = {
-            content = "",
+          window = {
+            mac_window_bar = true,
+            shadow = {
+              radius = 20,
+              color = "#00000040",
+            },
+            margin = {
+              x = 20,
+              y = 20,
+            },
+            border = {
+              width = 0,
+              color = "#ffffff30",
+            },
+            title_config = {
+              color = "#ffffff",
+              font_family = "SF Mono",
+            },
+            radius = 20,
           },
-          background = "#00000000",
         },
       })
     end,
