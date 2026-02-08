deefe1b10b0f94827ab7da6cf2d8042c32bfb3a6
ermengchao <zhangchao050530@gmail.com>
2026-02-08 16:43:45 +0800

    ✨ (*): introduce multiple updates

--- home/.chezmoihooks/sing-box.fish
+++ home/.chezmoihooks/sing-box.fish
@@ -1,10 +1,10 @@
 #!/usr/bin/env fish
 
 if type -q sing-box
-  cd $XDG_CONFIG_HOME/sing-box
-  set config_fields ./config
+    cd $XDG_CONFIG_HOME/sing-box
+    set config_fields ./config
 
-  if test -d $config_fields
-    sing-box merge config.json -C $config_fields
-  end
+    if test -d $config_fields
+        sing-box merge config.json -C $config_fields 2>/dev/null
+    end
 end
