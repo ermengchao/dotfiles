ed4961b544c70546e3e5b7f32143b561d2490a02
ermengchao <zhangchao050530@gmail.com>
2026-05-03 03:30:09 +0800

    ✨ (*): introduce multiple updates

--- home/dot_config/fish/conf.d/10-tmux.fish
+++ home/dot_config/fish/conf.d/10-tmux.fish
@@ -5,5 +5,10 @@ if status is-interactive
         test "$TERM_PROGRAM" = ghostty
         or set -q SSH_CONNECTION
     end
+    if set -q SSH_CONNECTION; and set -q IS_DARK_MODE
+        # Keep long-lived fish shells inside tmux aligned with the SSH client's theme.
+        set -Ux IS_DARK_MODE $IS_DARK_MODE
+    end
+
     exec tmux new-session -A -s main
 end
