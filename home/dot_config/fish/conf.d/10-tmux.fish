49b43238a3e824026147b0d39651e404b39fbf69
ermengchao <zhangchao050530@gmail.com>
2026-05-25 16:14:54 +0800

    ✨ (*): introduce multiple updates

--- home/dot_config/fish/conf.d/10-tmux.fish
+++ home/dot_config/fish/conf.d/10-tmux.fish
@@ -1,15 +1,14 @@
 if status is-interactive
-    and test -t 0
-    and test -t 1
-    and type -q tmux
-    and not set -q TMUX
-    and begin
-        test "$TERM_PROGRAM" = ghostty
-        or set -q SSH_CONNECTION
-    end
-    if set -q SSH_CONNECTION; and set -q IS_DARK_MODE
-        set -Ux IS_DARK_MODE $IS_DARK_MODE
-    end
-
-    exec tmux new-session -A -s main
+  and test -t 0
+  and test -t 1
+  and type -q tmux
+  and not set -q TMUX
+  and begin
+    test "$TERM_PROGRAM" = ghostty
+      or set -q SSH_CONNECTION
+  end
+  if set -q SSH_CONNECTION; and set -q IS_DARK_MODE
+    set -Ux IS_DARK_MODE $IS_DARK_MODE
+  end
+  exec tmux new-session -A -s main
 end
--- home/dot_config/fish/functions/glc.fish
+++ home/dot_config/fish/functions/glc.fish
@@ -1,16 +1,13 @@
 function glc --description "Create lefthook.yml from template"
-    set template ~/.config/fish/templates/lefthook.yml
-
-    if test -e lefthook.yml
-        echo "lefthook.yml already exists" >&2
-        return 1
-    end
-
-    if not test -e $template
-        echo "Template not found: $template" >&2
-        return 1
-    end
-
-    cp $template ./lefthook.yml
-    echo "Created lefthook.yml from template."
+  set template ~/.config/fish/templates/lefthook.yml
+  if test -e lefthook.yml
+    echo "lefthook.yml already exists" >&2
+    return 1
+  end
+  if not test -e $template
+    echo "Template not found: $template" >&2
+    return 1
+  end
+  cp $template ./lefthook.yml
+  echo "Created lefthook.yml from template."
 end
